65503cc83e9661e110b8776f77e28150de3714a9
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-04-26 21:46:16 +0700

    sh oneSiteLinksIntoHtml.sh <websitelink> giving a website with all links to library books

--- oneSiteLinksIntoHtml.sh
+++ oneSiteLinksIntoHtml.sh
@@ -1,9 +1,17 @@
 
 #!/bin/sh
 #
-html_name=$( echo "$1" | rev | gcut -s -d '/' -f -2 | rev | sed "s+/+.html+" )
+
+html_name () {
+    echo     $( echo "$1" | rev | gcut -s -d "/" -f -2 | rev | sed "s+/+.html+" )
+}
+
+# html_name=$( echo "$1" | rev | gcut -s -d "/" -f -2 | rev | sed "s+/+.html+" )
+html_name=$(html_name $1)
+
 cat start.txt > $html_name
 curl $1 | grep 'Link for access' | grep 'href[*]*' >> $html_name
 cat end.txt >> $html_name
 echo "open $html_name"
 open $html_name
+
